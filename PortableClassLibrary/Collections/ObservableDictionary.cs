7b9857355c0b2e80aaec2a4e2e74b1f4667ff213
Wim Van Laer <van.laer.wim@gmail.com>
2017-05-01 18:39:45 +0200

    Moved ObservableDictionary

--- PortableClassLibrary/Collections/ObservableDictionary.cs
+++ PortableClassLibrary/Collections/ObservableDictionary.cs
@@ -1,21 +1,20 @@
-﻿using ClassLibrary.Extensions;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using PortableClassLibrary.Extensions;
 
-namespace ClassLibrary.Collections
+
+namespace PortableClassLibrary.Collections
 {
     public class ObservableDictionary<TKey, TValue> : ObservableCollection<KeyValuePair<TKey, TValue>>,
         IDictionary<TKey, TValue>,
         IDictionary
     {
-        #region IMPLEMENTED
-
-
-
         #region IDICTIONARY<>
+
+        #region PROPERTIES
 
         public TValue this[TKey key]
         {
@@ -41,14 +40,13 @@ namespace ClassLibrary.Collections
             }
         }
 
-        public ICollection<TKey> Keys
-        {
-            get { return this.Select(x => x.Key).ToList(); }
-        }
-        public ICollection<TValue> Values
-        {
-            get { return this.Select(x => x.Value).ToList(); }
-        }
+        public ICollection<TKey> Keys => this.Select(x => x.Key).ToList();
+        public ICollection<TValue> Values => this.Select(x => x.Value).ToList();
+
+        #endregion PROPERTIES
+
+
+        #region METHODS
 
         public void Add(TKey key, TValue value)
         {
@@ -58,16 +56,13 @@ namespace ClassLibrary.Collections
             Add(new KeyValuePair<TKey, TValue>(key, value));
         }
 
-        public bool ContainsKey(TKey key)
-        {
-            return Keys.Contains(key);
-        }
+        public bool ContainsKey(TKey key) => Keys.Contains(key);
 
         public bool Remove(TKey key)
         {
             try
             {
-                int index = this.FindIndex(x => x.Key.Equals(key));
+                var index = this.FindIndex(x => x.Key.Equals(key));
 
                 if (index < 0)
                     return false;
@@ -95,16 +90,19 @@ namespace ClassLibrary.Collections
             }
         }
 
-        #endregion IDICTIONARY<>
+        #endregion METHODS
 
+        #endregion IDICTIONARY<>
 
 
         #region IDICTIONARY
 
+        #region PROPERTIES
+
         object IDictionary.this[object key]
         {
-            get { return this[(TKey)key]; }
-            set { this[(TKey)key] = (TValue)value; }
+            get => this[(TKey)key];
+            set => this[(TKey)key] = (TValue)value;
         }
 
         ICollection IDictionary.Keys => Keys.ToList();
@@ -113,10 +111,15 @@ namespace ClassLibrary.Collections
         public bool IsReadOnly => false;
         public bool IsFixedSize => false;
 
+        #endregion PROPERTIES
+
+
+        #region METHODS
+
         bool IDictionary.Contains(object key)
         {
             if (!(key is TKey))
-                throw (new ArgumentException("wrong type", nameof(key)));
+                throw new ArgumentException("wrong type", nameof(key));
 
             return Keys.Contains((TKey)key);
         }
@@ -124,30 +127,26 @@ namespace ClassLibrary.Collections
         void IDictionary.Add(object key, object value)
         {
             if (!(key is TKey))
-                throw (new ArgumentException("wrong type", nameof(key)));
+                throw new ArgumentException("wrong type", nameof(key));
             if (!(value is TValue))
-                throw (new ArgumentException("wrong type", nameof(value)));
+                throw new ArgumentException("wrong type", nameof(value));
 
             Add((TKey)key, (TValue)value);
         }
 
-        IDictionaryEnumerator IDictionary.GetEnumerator()
-        {
-            throw new NotImplementedException();
-        }
+        IDictionaryEnumerator IDictionary.GetEnumerator() => throw new NotImplementedException();
 
         void IDictionary.Remove(object key)
         {
             if (!(key is TKey))
-                throw (new ArgumentException("wrong type", nameof(key)));
+                throw new ArgumentException("wrong type", nameof(key));
 
             Remove((TKey)key);
         }
 
+        #endregion METHODS
+
+
         #endregion IDICTIONARY
-
-
-
-        #endregion IMPLEMENTED
     }
 }
