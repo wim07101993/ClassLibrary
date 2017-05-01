8bf47da2c9d3a15a10257c82a0b2ea5ccad73837
Wim Van Laer <van.laer.wim@gmail.com>
2017-05-01 18:33:28 +0200

    Moved DictionaryExtensions

--- PortableClassLibrary/Extensions/DictionaryExtensions.cs
+++ PortableClassLibrary/Extensions/DictionaryExtensions.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows;
 
-namespace ClassLibrary.Extensions
+
+namespace PortableClassLibrary.Extensions
 {
     public static class DictionaryExtensions
     {
