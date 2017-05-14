af93fe4c5bba0fb1605bf3e0f48a981bfbaa72da
Wim Van Laer <van.laer.wim@gmail.com>
2017-05-14 12:02:39 +0200

    different timer intervals added

--- Xamarin/Timer.cs
+++ Xamarin/Timer.cs
@@ -21,11 +21,41 @@ namespace ClassLibrary.Xamarin
                     Tick?.Invoke(null, EventArgs.Empty);
                     return true;
                 });
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(10), () =>
+            {
+                TenTicks?.Invoke(null, EventArgs.Empty);
+                return true;
+            });
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+            {
+                HundredTicks?.Invoke(null, EventArgs.Empty);
+                return true;
+            });
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                ThousandTicks?.Invoke(null, EventArgs.Empty);
+                return true;
+            });
         }
 
         /// <summary>
-        /// Occures when one millisecond has elapsed.
+        /// Occures when 1 millisecond has elapsed.
         /// </summary>
         public static event EventHandler Tick;
+        /// <summary>
+        /// Occures when 10 milliseconds has elapsed.
+        /// </summary>
+        public static event EventHandler TenTicks;
+        /// <summary>
+        /// Occures when 100 milliseconds has elapsed.
+        /// </summary>
+        public static event EventHandler HundredTicks;
+        /// <summary>
+        /// Occures when 1 second has elapsed.
+        /// </summary>
+        public static event EventHandler ThousandTicks;
     }
 }
