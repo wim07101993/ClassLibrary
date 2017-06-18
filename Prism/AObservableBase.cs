6224118abcb849cc40d36c096c873d8fbf4641bc
Wim Van Laer <van.laer.wim@gmail.com>
2017-06-18 20:43:07 +0200

    collections are listened to for undo in observable base

--- Prism/AObservableBase.cs
+++ Prism/AObservableBase.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using ClassLibrary.Portable.Extensions;
 using Prism.Mvvm;
+
+
+// ReSharper disable ExplicitCallerInfoArgument
 
 
 namespace ClassLibrary.Prism
@@ -13,32 +18,43 @@ namespace ClassLibrary.Prism
     {
         #region FIELDS
 
+        /// <summary>
+        /// Dictionary to store all old values of properties
+        /// </summary>
         private Dictionary<string, IList> _oldValueDictionary;
 
         #endregion FIELDS
 
 
+        #region PROPERTIES
+
+        /// <summary>
+        /// Read-only-collection of all the properties that have been changed;
+        /// </summary>
+        public IReadOnlyCollection<string> ChangedProperties
+            => new ReadOnlyCollection<string>(_oldValueDictionary.Keys.ToList());
+
+        #endregion PROPERTIES
+
+
         #region METHODS
 
+        /// <summary>
+        /// Sets the <see cref="storage"/> to the value <see cref="value"/> if the value and storage are not equal and raises propertyChanged
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="storage"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
         protected override bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
         {
             // if the value equals the storage value or there is no propertyName, there is nothing to change
             if (Equals(storage, value) || string.IsNullOrWhiteSpace(propertyName))
                 return false;
 
-            // check if old value dictionary exists and add the new value to it
-            if (_oldValueDictionary == null)
-                _oldValueDictionary = new Dictionary<string, IList>
-                {
-                    {
-                        propertyName,
-                        new List<object> {storage}
-                    }
-                };
-            else if (_oldValueDictionary.ContainsKey(propertyName))
-                _oldValueDictionary[propertyName].Add(storage);
-            else
-                _oldValueDictionary.Add(propertyName, new List<object> {storage});
+            // add old value to storage
+            AddValueToOldValueDictionary(propertyName, value);
 
             // set the storage to the new value
             storage = value;
@@ -49,6 +65,77 @@ namespace ClassLibrary.Prism
             return true;
         }
 
+        /// <summary>
+        /// Sets an <see cref="ObservableCollection{T}"/> to the value <see cref="value"/> if the value and storage are not equal.
+        /// Subscribes to collectionChanged to add undo ability.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="storage"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected virtual bool SetCollection<T>(ref ObservableCollection<T> storage, ObservableCollection<T> value,
+            [CallerMemberName] string propertyName = null)
+        {
+            // if the value equals the storage value or there is no propertyName, there is nothing to change
+            if (Equals(storage, value) || string.IsNullOrWhiteSpace(propertyName))
+                return false;
+
+            // add old value to storage
+            AddValueToOldValueDictionary(propertyName, storage);
+
+            // if storage is not null, unsubscribe from CollectionChanged
+            if (storage != null)
+                storage.CollectionChanged -= StorageOnCollectionChanged;
+
+            // set the storage to the new value
+            storage = value;
+
+            // if storage not is null, subscribe to CollectionChanged
+            if (storage != null)
+                storage.CollectionChanged += StorageOnCollectionChanged;
+
+            // raise a property changed
+            RaisePropertyChanged(propertyName);
+            APropertyChanged(propertyName);
+
+            return true;
+
+            // method to add collection to old value dictionary
+            void StorageOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
+                => AddValueToOldValueDictionary(propertyName, value);
+        }
+
+        /// <summary>
+        /// Adds a value to the Old Value Dictionary. If the dictionary doesn't exist yet, it is created.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <param name="value"></param>
+        private void AddValueToOldValueDictionary(string propertyName, object value)
+        {
+            // if dictionary doesn't exits, create it with the entry
+            if (_oldValueDictionary == null)
+                _oldValueDictionary = new Dictionary<string, IList>
+                {
+                    {
+                        propertyName,
+                        new List<object> {value}
+                    }
+                };
+            // if dictionary already has values for propertyName add value to values list
+            else if (_oldValueDictionary.ContainsKey(propertyName))
+                _oldValueDictionary[propertyName].Add(value);
+            // else just add new entry
+            else
+                _oldValueDictionary.Add(propertyName, new List<object> {value});
+        }
+
+        /// <summary>
+        /// Resets property to the last time the property was set with the <see cref="SetProperty{T}"/> 
+        /// method or <see cref="SetCollection{T}"/> method.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
         public bool Undo(string propertyName)
         {
             // if there is no dictionary, there is nothing to undo
@@ -85,7 +172,11 @@ namespace ClassLibrary.Prism
             return true;
         }
 
-        public virtual void APropertyChanged(string propertyName)
+        /// <summary>
+        /// Method that gets called when a property changed.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected virtual void APropertyChanged(string propertyName)
         {
         }
 
